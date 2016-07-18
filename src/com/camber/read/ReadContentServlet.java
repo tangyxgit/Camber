ecedff269db62e6e7c74054c1a05963d57936e28
tangyx <tangyx@live.com>
2016-07-18 21:48:29 +0800

    xiedong

--- src/com/camber/read/ReadContentServlet.java
+++ src/com/camber/read/ReadContentServlet.java
@@ -25,6 +25,7 @@ public class ReadContentServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		System.out.println("----");
 	}
 
 	/**
